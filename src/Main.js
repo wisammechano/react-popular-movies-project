81fdd220d97c119f703b0fbb49ea10ca86ce5def
Wisam Naji <guitar90rocker@gmail.com>
2019-12-25 02:17:34 +0300

    cleaned up initial projects files

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+class Main extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default Main;
