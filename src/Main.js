74c2f35ffd7d7ac8d59853a284197fc75259a91b
Wisam Naji <guitar90rocker@gmail.com>
2019-12-25 15:50:07 +0300

    connected the maon component to the store

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import "./Main.css";
 
+import { connect } from "react-redux";
+
+const mapStateToProps = (state, ownProps) => ({});
+
+const mapDispatchToProps = dispatch => ({});
 class Main extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
   }
 }
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
