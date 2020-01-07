dcefbe3946f7940259338a814389dbb626e892d1
Wisam Naji <guitar90rocker@gmail.com>
2020-01-07 19:13:29 +0300

    Renamed Pages

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = props => {
+const HomePage = props => {
   return <div>Home</div>;
 };
 
-export default Home;
+export default HomePage;
---
+++ src/components/MoviePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MoviePage = props => {
+  return <div>MoviePage</div>;
+};
+
+export default MoviePage;
